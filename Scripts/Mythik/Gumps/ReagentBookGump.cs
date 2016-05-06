1419058b3a6471b6dea43e62a40edb6aa3ec7688
James Kidd <kklolk@gmail.com>
2016-05-06 22:50:48 +1000

    ReagentBook ( thanks Lux )

---
+++ Scripts/Mythik/Gumps/ReagentBookGump.cs
@@ -0,0 +1,243 @@
+﻿using Scripts.Mythik.Items.Craftables.Inscription;
+using Scripts.Mythik.Items.Misc;
+using Server;
+using Server.Gumps;
+using Server.Items;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Scripts.Mythik.Gumps
+{
+    public enum ReagentBookGumpPage
+    {
+        Regular = 0,
+        Pagan,
+    }
+
+    public class ReagentBookGump : Gump
+    {
+        private Mobile m_Mobile;
+        private ReagentBook m_Book;
+
+        private int m_defValue;
+
+        private static ReagentEntry[] m_Regular = new ReagentEntry[] {
+                                                                         new ReagentEntry( 0, 0xF7A, "Black pearl" ),
+                                                                         new ReagentEntry( 1, 0xF7B, "Bloodmoss" ),
+                                                                         new ReagentEntry( 2, 0xF84, "Garlic" ),
+                                                                         new ReagentEntry( 3, 0xF85, "Ginseng" ),
+                                                                         new ReagentEntry( 4, 0xF86, "Mandrake root" ),
+                                                                         new ReagentEntry( 5, 0xF88, "Nightshade" ),
+                                                                         new ReagentEntry( 6, 0xF8D, "Spider's silk" ),
+                                                                         new ReagentEntry( 7, 0xF8C, "Sulfurous ash" )
+                                                                     };
+
+        private static ReagentEntry[] m_Pagan = new ReagentEntry[] {
+                                                                       new ReagentEntry( 8, 0xF78, "Bat wing" ),
+                                                                       new ReagentEntry( 9, 0xF7D, "Daemon blood" ),
+                                                                       new ReagentEntry( 10, 0xF80, "Daemon bone" ),
+                                                                       new ReagentEntry( 11, 0xF87, "Eye of newt" ),
+                                                                       new ReagentEntry( 12, 0xF8F, "Grave dust" ),
+                                                                       new ReagentEntry( 13, 0xF8E, "Nox crystal" ),
+                                                                       new ReagentEntry( 14, 0xF8A, "Pig iron" )
+                                                                   };
+
+        public ReagentBookGump(Mobile mobile, ReagentBook book, ReagentBookGumpPage page, int defValue) : base(150, 200)
+        {
+            mobile.CloseGump(typeof(ReagentBookGump));
+
+            m_Mobile = mobile;
+            m_Book = book;
+
+            m_defValue = defValue;
+
+            ReagentEntry[] list = m_Regular;
+
+            if (page == ReagentBookGumpPage.Pagan)
+                list = m_Pagan;
+
+            AddPage(0);
+
+            AddBackground(10, 10, 444, 439, 5054);
+            AddImageTiled(18, 20, 444 - 17, 420, 2624);
+
+            AddImageTiled(48, 64, 36, 20, 200);
+            AddImageTiled(96, 64, 133, 20, 1416);
+            AddImageTiled(231, 64, 80, 20, 200);
+            AddImageTiled(313, 64, 75, 20, 1416);
+
+            AddImageTiled(48, 90, 36, 210, 200);
+            AddImageTiled(96, 90, 133, 210, 1416);
+            AddImageTiled(231, 90, 80, 210, 200);
+            AddImageTiled(313, 90, 75, 210, 1416);
+
+            AddAlphaRegion(18, 20, 444 - 17, 420);
+
+            AddImage(5, 5, 10460);
+            AddImage(444 - 15, 5, 10460);
+            AddImage(5, 424, 10460);
+            AddImage(444 - 15, 424, 10460);
+
+            AddHtml(255, 64, 200, 32, "<BASEFONT COLOR=\"#FFFFFF\">Held</BASEFONT>", false, false); // Held
+            AddHtml(330, 64, 200, 32, "<BASEFONT COLOR=\"#FFFFFF\">Amount</BASEFONT>", false, false); // Amount
+
+            if (page == ReagentBookGumpPage.Regular)
+            {
+                AddImage(49, 66, 0x25EA);
+                AddButton(67, 66, 0x15E1, 0x15E5, GetButtonID(0, 1), GumpButtonType.Reply, 0);
+
+                AddHtml(96, 64, 133, 32, "<BASEFONT COLOR=\"#FFFFFF\"><CENTER>Regular Reagents</CENTER></BASEFONT>", false, false); // Reagent
+            }
+            else
+            {
+                AddButton(49, 66, 0x15E3, 0x15E7, GetButtonID(0, 2), GumpButtonType.Reply, 0);
+                AddImage(67, 66, 0x25E6);
+
+                AddHtml(96, 64, 133, 32, "<BASEFONT COLOR=\"#FFFFFF\"><CENTER>Pagan Reagent</CENTER></BASEFONT>", false, false); // Reagent
+            }
+
+            for (int i = 0, j = 0; i < list.Length; ++i, ++j)
+            {
+                ReagentEntry entry = list[i];
+
+                int count = book.Reagents[entry.Index];
+                int y = 100 + (25 * i);
+
+                AddItem(45, y + 5, entry.ItemID);
+                AddLabel(100, y, 1153, entry.Name);
+                AddLabel(235, y, 1153, count.ToString());
+
+                if (count > 0)
+                {
+                    AddTextBox(325, y, 50, 20, entry.Index, defValue.ToString());
+                    AddButton(388, y - 2, 4005, 4007, GetButtonID(1, entry.Index), GumpButtonType.Reply, 0);
+                }
+            }
+        }
+
+        public override void OnResponse(Server.Network.NetState sender, RelayInfo Info)
+        {
+            int val = Info.ButtonID - 1;
+
+            if (val < 0)
+                return;
+
+            int type = val % 10;
+            int index = val / 10;
+
+            switch (type)
+            {
+                case 0:
+                    {
+                        switch (index)
+                        {
+                            case 1: { m_Mobile.SendGump(new ReagentBookGump(m_Mobile, m_Book, ReagentBookGumpPage.Pagan, m_defValue)); break; }
+                            case 2: { m_Mobile.SendGump(new ReagentBookGump(m_Mobile, m_Book, ReagentBookGumpPage.Regular, m_defValue)); break; }
+                        }
+
+                        break;
+                    }
+                case 1:
+                    {
+                        if (index >= 0 && index <= 14)
+                        {
+                            int count = Utility.ToInt32(Info.GetTextEntry(index).Text);
+
+                            if (count > 0)
+                            {
+                                if (count <= m_Book.Reagents[index])
+                                {
+                                    BaseReagent reagent;
+                                    bool added = false;
+
+                                    try { reagent = (BaseReagent)Activator.CreateInstance(GetReagent(index)); }
+                                    catch { reagent = null; }
+
+                                    if (reagent != null)
+                                    {
+                                        reagent.Amount = count;
+
+                                        if ((added = m_Mobile.AddToBackpack(reagent)))
+                                            m_Mobile.SendMessage("The reagents have been added into your backpack.");
+                                        else
+                                            m_Mobile.SendMessage("The book was unable to give you the reagents. Your backpack may contain too many items.");
+
+                                        if (!added)
+                                            reagent.Delete();
+                                        else
+                                            m_Book.Reagents[index] -= count;
+
+                                        m_Book.InvalidateProperties();
+                                    }
+                                }
+                                else
+                                {
+                                    m_Mobile.SendMessage("The book does not contain that many of those reagents.");
+                                }
+                            }
+                        }
+
+                        break;
+                    }
+            }
+        }
+
+        public static Type GetReagent(int index)
+        {
+            switch (index)
+            {
+                case 0: return typeof(BlackPearl);
+                case 1: return typeof(Bloodmoss);
+                case 2: return typeof(Garlic);
+                case 3: return typeof(Ginseng);
+                case 4: return typeof(MandrakeRoot);
+                case 5: return typeof(Nightshade);
+                case 6: return typeof(SpidersSilk);
+                case 7: return typeof(SulfurousAsh);
+                case 8: return typeof(BatWing);
+                case 9: return typeof(DaemonBlood);
+                case 10: return typeof(DaemonBone);
+                case 11: return typeof(EyeOfNewt);
+                case 12: return typeof(GraveDust);
+                case 13: return typeof(NoxCrystal);
+                case 14: return typeof(PigIron);
+            }
+
+            return typeof(BlackPearl);
+        }
+
+        public void AddTextBox(int x, int y, int width, int height, int id, string def)
+        {
+            AddImageTiled(x - 2, y, width + 4, height - 2, 0xBBC);
+            AddImageTiled(x - 1, y + 1, width + 2, height - 4, 2624);
+            AddAlphaRegion(x - 1, y + 1, width + 2, height - 4);
+            AddTextEntry(x, y, width - 2, height, 0x481, id, def);
+        }
+
+        public int GetButtonID(int type, int index)
+        {
+            return 1 + (index * 10) + type;
+        }
+
+        private class ReagentEntry
+        {
+            private int m_Index;
+            private int m_ItemID;
+            private string m_Name;
+
+            public int Index { get { return m_Index; } }
+            public int ItemID { get { return m_ItemID; } }
+            public string Name { get { return m_Name; } }
+
+            public ReagentEntry(int index, int itemID, string name)
+            {
+                m_Index = index;
+                m_ItemID = itemID;
+                m_Name = name;
+            }
+        }
+    }
+}
---
+++ Scripts/Mythik/Items/Craftables/Inscription/ReagentBook.cs
@@ -0,0 +1,212 @@
+﻿using Scripts.Mythik.Gumps;
+using Scripts.Mythik.Items.Misc;
+using Server;
+using Server.Items;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Scripts.Mythik.Items.Craftables.Inscription
+{
+    //Thanks lux
+    public class ReagentBook : Item
+    {
+        private int[] m_Reagents;
+
+        public int BlackPearl { get { return m_Reagents[0]; } set { m_Reagents[0] = value; InvalidateProperties(); } }
+        public int Bloodmoss { get { return m_Reagents[1]; } set { m_Reagents[1] = value; InvalidateProperties(); } }
+        public int Garlic { get { return m_Reagents[2]; } set { m_Reagents[2] = value; InvalidateProperties(); } }
+        public int Ginseng { get { return m_Reagents[3]; } set { m_Reagents[3] = value; InvalidateProperties(); } }
+        public int MandrakeRoot { get { return m_Reagents[4]; } set { m_Reagents[4] = value; InvalidateProperties(); } }
+        public int Nightshade { get { return m_Reagents[5]; } set { m_Reagents[5] = value; InvalidateProperties(); } }
+        public int SpidersSilk { get { return m_Reagents[6]; } set { m_Reagents[6] = value; InvalidateProperties(); } }
+        public int SulfurousAsh { get { return m_Reagents[7]; } set { m_Reagents[7] = value; InvalidateProperties(); } }
+
+        public int BatWing { get { return m_Reagents[8]; } set { m_Reagents[8] = value; InvalidateProperties(); } }
+        public int DaemonBlood { get { return m_Reagents[9]; } set { m_Reagents[9] = value; InvalidateProperties(); } }
+        public int DaemonBone { get { return m_Reagents[10]; } set { m_Reagents[10] = value; InvalidateProperties(); } }
+        public int EyeOfNewt { get { return m_Reagents[11]; } set { m_Reagents[11] = value; InvalidateProperties(); } }
+        public int GraveDust { get { return m_Reagents[12]; } set { m_Reagents[12] = value; InvalidateProperties(); } }
+        public int NoxCrystal { get { return m_Reagents[13]; } set { m_Reagents[13] = value; InvalidateProperties(); } }
+        public int PigIron { get { return m_Reagents[14]; } set { m_Reagents[14] = value; InvalidateProperties(); } }
+
+        public int[] Reagents { get { return m_Reagents; } set { m_Reagents = value; } }
+
+
+        [Constructable]
+        public ReagentBook(int amount) : base(0x2253)
+        {
+            this.Name = "reagent book";
+            m_Reagents = new int[15];
+
+            for (int i = 0; i < 15; ++i)
+                m_Reagents[i] = amount;
+        }
+
+        public ReagentBook(Serial serial) : base(serial)
+        {
+        }
+
+        public override void OnDoubleClick(Mobile from)
+        {
+            if (from.InRange(GetWorldLocation(), 1))
+                from.SendGump(new ReagentBookGump(from, this, ReagentBookGumpPage.Regular, 0));
+
+        }
+
+        public override bool OnDragDrop(Mobile from, Item dropped)
+        {
+            if (dropped is BaseReagent)
+            {
+                return AddReagent(from, (BaseReagent)dropped, false);
+            }
+            else if (dropped is Container)
+            {
+                Container container = (Container)dropped;
+
+                ArrayList toDelete = new ArrayList();
+                bool added = false;
+
+                for (int i = 0; i < container.Items.Count; ++i)
+                {
+                    Item item = (Item)container.Items[i];
+
+                    if (item is BaseReagent)
+                    {
+                        if ((added = AddReagent(from, (BaseReagent)item, true)))
+                            toDelete.Add(item);
+                    }
+                }
+
+                if (added)
+                    from.SendMessage("You add reagents from the container into the book.");
+
+                for (int j = 0; j < toDelete.Count; ++j)
+                    ((Item)toDelete[j]).Delete();
+
+                if (container.Items.Count == 0)
+                    return true;
+            }
+            else
+            {
+                from.SendLocalizedMessage(1042276); // You cannot drop that there.
+            }
+
+            return base.OnDragDrop(from, dropped);
+        }
+
+        public bool AddReagent(Mobile from, BaseReagent reagent, bool passive)
+        {
+            int index = GetIndex(reagent);
+
+            int count = m_Reagents[index];
+            int amount = reagent.Amount;
+
+            int total = count + amount;
+
+            if (count >= 50000)
+            {
+                if (!passive)
+                    from.SendMessage("This book cannot hold any more of that reagent.");
+            }
+            else
+            {
+                bool action;
+
+                if (total > 50000)
+                {
+                    int excess = total - 50000;
+                    int toAdd = amount - excess;
+
+                    m_Reagents[index] += toAdd;
+                    reagent.Amount -= toAdd;
+
+                    if (!passive)
+                        from.SendMessage("You were only able to add {0} of the reagents.", toAdd);
+
+                    action = false;
+                }
+                else
+                {
+                    m_Reagents[index] += amount;
+
+                    if (!passive)
+                        from.SendMessage("You add all of the reagents into the book.");
+
+                    action = true;
+                }
+
+                InvalidateProperties();
+
+                return action;
+            }
+
+            return false;
+        }
+
+        public static int GetIndex(BaseReagent reagent)
+        {
+            if (reagent is BlackPearl) { return 0; }
+            else if (reagent is Bloodmoss) { return 1; }
+            else if (reagent is Garlic) { return 2; }
+            else if (reagent is Ginseng) { return 3; }
+            else if (reagent is MandrakeRoot) { return 4; }
+            else if (reagent is Nightshade) { return 5; }
+            else if (reagent is SpidersSilk) { return 6; }
+            else if (reagent is SulfurousAsh) { return 7; }
+
+            else if (reagent is BatWing) { return 8; }
+            else if (reagent is DaemonBlood) { return 9; }
+            else if (reagent is DaemonBone) { return 10; }
+            else if (reagent is EyeOfNewt) { return 11; }
+            else if (reagent is GraveDust) { return 12; }
+            else if (reagent is NoxCrystal) { return 13; }
+            else if (reagent is PigIron) { return 14; }
+
+            return 0;
+        }
+
+        public override void GetProperties(ObjectPropertyList list)
+        {
+            base.GetProperties(list);
+
+            for (int i = 0; i < 15; ++i)
+                if (m_Reagents[i] > 0)
+                    list.Add(1080521 + i, "{0}", m_Reagents[i]);
+        }
+
+
+        public override void Serialize(GenericWriter writer)
+        {
+            base.Serialize(writer);
+
+            writer.Write((int)0); // version
+
+            for (int i = 0; i < 15; ++i)
+                writer.Write(m_Reagents[i]);
+        }
+
+        public override void Deserialize(GenericReader reader)
+        {
+            base.Deserialize(reader);
+
+            int version = reader.ReadInt();
+
+            switch (version)
+            {
+                case 0:
+                    {
+                        m_Reagents = new int[15];
+
+                        for (int i = 0; i < 15; ++i)
+                            m_Reagents[i] = reader.ReadInt();
+
+                        break;
+                    }
+            }
+        }
+    }
+}
