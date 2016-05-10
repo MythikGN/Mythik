f5c357f2611cdbcdc4efe5afd83ffa9105041c8d
James Kidd <kklolk@gmail.com>
2016-05-10 15:00:10 +1000

    Disable elfs

--- Scripts/Misc/CharacterCreation.cs
+++ Scripts/Misc/CharacterCreation.cs
@@ -636,9 +636,9 @@ namespace Server.Misc
 			newChar.Female = args.Female;
 			//newChar.Body = newChar.Female ? 0x191 : 0x190;
 
-			if( Core.Expansion >= args.Race.RequiredExpansion )
-				newChar.Race = args.Race;	//Sets body
-			else
+			//if( Core.Expansion >= args.Race.RequiredExpansion )
+			//	newChar.Race = args.Race;	//Sets body
+			//else
 				newChar.Race = Race.DefaultRace;
 
 			//newChar.Hue = Utility.ClipSkinHue( args.Hue & 0x3FFF ) | 0x8000;
