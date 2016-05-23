b4ebbf289be107cce922fc3cce69ee1361173331
James Kidd <kklolk@gmail.com>
2016-05-23 23:48:18 +1000

    Achievement system, work in progress. still need to finish gump.

---
+++ Scripts/Mythik/Systems/Achievements/AcheivmentSystem.cs
@@ -0,0 +1,89 @@
+﻿//#define STOREONITEM
+
+using Server;
+using System;
+using System.Collections.Generic;
+
+using Scripts.Mythik.Mobiles;
+using Scripts.Mythik.Systems.Achievements.Gumps;
+using Server.Mobiles;
+using Server.Items;
+using Server.Commands;
+using Server.Misc;
+
+
+namespace Scripts.Mythik.Systems.Achievements
+{
+
+    public class AcheivmentSystem
+    {
+        public static List<Achievement> Achievements = new List<Achievement>();
+        public static void Initialize()
+        {
+            Achievements.Add(new HunterAchievement(1, 0, 0, false, 5, "Dog Slayer", "Slay 5 Dogs", 5, typeof(Dog), typeof(Mythik.Items.Rares.RareClothDyeTub)));
+            Achievements.Add(new DiscoveryAchievement(2, 0, 0x96E, false, 1, "Minoc!", "Discover Minoc Township", 5, "Minoc"));
+            Achievements.Add(new HarvestAchievement(3, 0, 0, false, 500, "500 Iron Ore", "Mine 500 Iron Ore", 5, typeof(IronOre), typeof(AncientSmithyHammer)));
+            Achievements.Add(new DiscoveryAchievement(4, 0, 0, false, 1, "Yew!", "Discover the Yew Township", 5, "Yew"));
+
+            CommandSystem.Register("feats", AccessLevel.Player, new CommandEventHandler(OpenGump));
+
+        }
+
+        private static void OpenGump(CommandEventArgs e)
+        {
+            var player = e.Mobile as PlayerMobile;
+            if(player != null)
+                e.Mobile.SendGump(new AchievementGump(player));
+        }
+
+        internal static void SetAchievementStatus(PlayerMobile player, Achievement ach, int progress)
+        {
+
+#if STOREONITEM
+           if (!AcheivmentSystemStone.GetInstance().Achievements.ContainsKey(player.Serial))
+                AcheivmentSystemStone.GetInstance().Achievements.Add(player.Serial, new Dictionary<int, AcheiveData>());
+            var achieves = AcheivmentSystemStone.GetInstance().Achievements[player.Serial];
+#else
+            var achieves = (player as MythikPlayerMobile).Achievements;
+#endif
+
+
+            if (achieves.ContainsKey(ach.ID))
+            {
+                if (achieves[ach.ID].Progress >= ach.CompletionTotal)
+                    return;
+                achieves[ach.ID].Progress += progress;
+            }
+            else
+            {
+                achieves.Add(ach.ID, new AcheiveData() { Progress = progress });
+            }
+
+            if (achieves[ach.ID].Progress >= ach.CompletionTotal) {
+                player.SendGump(new AchievementObtainedGump(ach),false);
+                achieves[ach.ID].CompletedOn = DateTime.Now;
+
+
+#if STOREONITEM
+                if (!AcheivmentSystemStone.GetInstance().PointsTotals.ContainsKey(player.Serial))
+                    AcheivmentSystemStone.GetInstance().PointsTotals.Add(player.Serial, 0);
+                AcheivmentSystemStone.GetInstance().PointsTotals[player.Serial] += ach.RewardPoints;
+#else
+                (player as MythikPlayerMobile).AchievementPointsTotal += ach.RewardPoints;
+#endif
+                if (ach.RewardItems?.Length > 0)
+                {
+                    try
+                    {
+                        var item = (Item)Activator.CreateInstance(ach.RewardItems[0]);
+                        if (!WeightOverloading.IsOverloaded(player))
+                            player.Backpack.DropItem(item);
+                        else
+                            player.BankBox.DropItem(item);
+                    }
+                    catch { }
+                }
+            }
+        }
+    }
+}
---
+++ Scripts/Mythik/Systems/Achievements/Gumps/AchievementGump.cs
@@ -0,0 +1,53 @@
+﻿using Scripts.Mythik.Mobiles;
+using Server;
+using Server.Gumps;
+using Server.Mobiles;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Scripts.Mythik.Systems.Achievements.Gumps
+{
+    class AchievementGump : Gump
+    {
+        int FORWARD = 1;
+        int BACK = 2;
+        public AchievementGump(PlayerMobile m) : base(25,25)
+        {
+            var player = m as MythikPlayerMobile;
+            this.Closable = true;
+            this.Disposable = true;
+            this.Dragable = true;
+            this.Resizable = false;
+            this.AddPage(0);
+            this.AddBackground(11, 15, 758, 575, 2600);
+            this.AddBackground(57, 92, 666, 478, 9250);
+            this.AddBackground(321, 104, 386, 453, 9270);
+            this.AddBackground(72, 104, 245, 453, 9270);
+            this.AddBackground(72, 34, 635, 53, 9270);
+            this.AddBackground(327, 0, 133, 41, 9200);
+            this.AddLabel(292, 52, 68, @"Mythik Achievment System");
+            this.AddLabel(360, 11, 82, player.AchievementPointsTotal + @" Points");
+            this.AddBackground(341, 522, 353, 26, 9200);
+            this.AddButton(658, 524, 4005, 248, FORWARD, GumpButtonType.Reply, 0);
+            this.AddButton(345, 524, 4014, 248, FORWARD, GumpButtonType.Page, 0);
+
+            this.AddBackground(90, 123, 209, 25, 9200);
+           // this.AddButton(102, 129, 1209, 1210, (int)Buttons.catAbtn, GumpButtonType.Reply, 0);
+            this.AddLabel(122, 125, 0, @"Category A");
+
+            this.AddBackground(91, 154, 209, 25, 9200);
+            //this.AddButton(103, 160, 1210, 1210, (int)Buttons.catBbtn, GumpButtonType.Reply, 0);
+            this.AddLabel(123, 156, 249, @"Category B");
+
+            this.AddBackground(123, 187, 155, 25, 9200);
+           // this.AddButton(135, 193, 1209, 1210, (int)Buttons.subCatbtn, GumpButtonType.Reply, 0);
+            this.AddLabel(155, 189, 0, @"SubCategory C");
+
+
+
+        }
+    }
+}
