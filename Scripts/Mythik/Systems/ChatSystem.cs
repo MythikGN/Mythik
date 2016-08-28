fb889b2ea89499f345006f73e7f75ec60b153430
James Kidd <kklolk@gmail.com>
2016-08-28 20:10:09 +1000

    Basic chat system.

---
+++ Scripts/Mythik/Systems/ChatSystem.cs
@@ -0,0 +1,60 @@
+﻿using Scripts.Mythik.Mobiles;
+using Server;
+using Server.Commands;
+using Server.Network;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Scripts.Mythik.Systems
+{
+   public class ChatSystem
+    {
+        public static void Initialize()
+        {
+            CommandSystem.Register("chaton", AccessLevel.Player, new CommandEventHandler(EnableChat));
+            CommandSystem.Register("chatoff", AccessLevel.Player, new CommandEventHandler(DisableChat));
+            CommandSystem.Register("c", AccessLevel.Player, new CommandEventHandler(SendChatMessage));
+            CommandSystem.Register("chat", AccessLevel.Player, new CommandEventHandler(SendChatMessage));
+            CommandSystem.Register("shout", AccessLevel.Player, new CommandEventHandler(SendChatMessage));
+        }
+
+        private static void SendChatMessage(CommandEventArgs e)
+        {
+            var msg = "[" + e.Mobile.Name + "]: " + e.ArgString;
+            var hue = 0xb2;
+            if (e.Mobile.AccessLevel > AccessLevel.Player)
+                hue = 0x44;
+            Packet p = new AsciiMessage(Serial.MinusOne, -1, MessageType.Regular, hue, 3, "System", msg);
+            
+
+            List<NetState> list = NetState.Instances;
+
+            p.Acquire();
+
+            for (int i = 0; i < list.Count; ++i)
+            {
+                if (list[i].Mobile != null && ((MythikPlayerMobile)list[i].Mobile).ChatEnabled)
+                    list[i].Send(p);
+            }
+
+            p.Release();
+
+            NetState.FlushAll();
+        }
+      
+
+
+        private static void DisableChat(CommandEventArgs e)
+        {
+            ((MythikPlayerMobile)e.Mobile).ChatEnabled = false;
+        }
+
+        private static void EnableChat(CommandEventArgs e)
+        {
+            ((MythikPlayerMobile)e.Mobile).ChatEnabled = true;
+        }
+    }
+}
