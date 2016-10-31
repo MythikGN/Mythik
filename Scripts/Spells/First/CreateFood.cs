2cbf6a2b4841e32984e58120ae92c57a674b6c5b
James Kidd <kklolk@gmail.com>
2016-10-31 16:52:39 +1100

    CreateFood cleaned up.

--- Scripts/Spells/First/CreateFood.cs
+++ Scripts/Spells/First/CreateFood.cs
@@ -3,91 +3,115 @@ using Server.Items;
 
 namespace Server.Spells.First
 {
-	public class CreateFoodSpell : MagerySpell
-	{
-		private static SpellInfo m_Info = new SpellInfo(
-				"Create Food", "In Mani Ylem",
-				224,
-				9011,
-				Reagent.Garlic,
-				Reagent.Ginseng,
-				Reagent.MandrakeRoot
-			);
+    public class CreateFoodSpell : MagerySpell
+    {
+        public override SpellCircle Circle { get { return SpellCircle.First; } }
+        public override int Sound { get { return 0x1E2; } }
 
-		public override SpellCircle Circle { get { return SpellCircle.First; } }
-        public override bool HasNoTarget
+        public override bool CanTargetGround { get { return true; } }
+
+        private static readonly SpellInfo m_Info = new SpellInfo(
+                "Create Food", "In Mani Ylem",
+                263,
+                9011,
+                Reagent.Garlic,
+                Reagent.Ginseng,
+                Reagent.MandrakeRoot
+            );
+
+        public CreateFoodSpell(Mobile caster, Item scroll) : base(caster, scroll, m_Info)
         {
-            get { return true; }
         }
-        public CreateFoodSpell( Mobile caster, Item scroll ) : base( caster, scroll, m_Info )
-		{
-		}
 
-		private static FoodInfo[] m_Food = new FoodInfo[]
-			{
-				new FoodInfo( typeof( Grapes ), "a grape bunch" ),
-				new FoodInfo( typeof( Ham ), "a ham" ),
-				new FoodInfo( typeof( CheeseWedge ), "a wedge of cheese" ),
-				new FoodInfo( typeof( Muffins ), "muffins" ),
-				new FoodInfo( typeof( FishSteak ), "a fish steak" ),
-				new FoodInfo( typeof( Ribs ), "cut of ribs" ),
-				new FoodInfo( typeof( CookedBird ), "a cooked bird" ),
-				new FoodInfo( typeof( Sausage ), "sausage" ),
-				new FoodInfo( typeof( Apple ), "an apple" ),
-				new FoodInfo( typeof( Peach ), "a peach" )
-			};
+        private static readonly FoodInfo[] m_Food = new[]
+                                                        {
+                                                            new FoodInfo(typeof (Grapes), "a grape bunch"),
+                                                            new FoodInfo(typeof (Ham), "a ham"),
+                                                            new FoodInfo(typeof (CheeseWedge), "a wedge of cheese"),
+                                                            new FoodInfo(typeof (Muffins), "a muffin"),
+                                                            new FoodInfo(typeof (FishSteak), "a fish steak"),
+                                                            new FoodInfo(typeof (Ribs), "a cut of ribs"),
+                                                            new FoodInfo(typeof (CookedBird), "a cooked bird"),
+                                                            new FoodInfo(typeof (Sausage), "a sausage"),
+                                                            new FoodInfo(typeof (Apple), "an apple"),
+                                                            new FoodInfo(typeof (Peach), "a peach"),
+                                                            new FoodInfo(typeof (Cake), "a cake"),
+                                                        };
 
-		public override void OnCast()
-		{
-			if ( CheckSequence() )
-			{
-				FoodInfo foodInfo = m_Food[Utility.Random( m_Food.Length )];
-				Item food = foodInfo.Create();
+        public override void OnPlayerCast()
+        {
+            if (SphereSpellTarget is IPoint3D)
+                Target((IPoint3D)SphereSpellTarget);
+            else
+                DoFizzle();
+        }
 
-				if ( food != null )
-				{
-					Caster.AddToBackpack( food );
+        public override void OnCast()
+        {
+            Target(Caster.Location);
+        }
 
-					// You magically create food in your backpack:
-					Caster.SendLocalizedMessage( 1042695, true, " " + foodInfo.Name );
+        public void Target(IPoint3D p)
+        {
+            if (!Caster.CanSee(p))
+            {
+                Caster.SendAsciiMessage("Target is not in line of sight.");
+                DoFizzle();
+            }
+            else if (SphereSpellTarget is BaseWand)
+            {
+                BaseWand bw = SphereSpellTarget as BaseWand;
+                bw.RechargeWand(Caster, this);
+            }
+            else if (CheckSequence())
+            {
+                FoodInfo foodInfo = m_Food[Utility.Random(m_Food.Length)];
+                Item food = foodInfo.Create();
+                Point3D loc = new Point3D(p);
 
-					Caster.FixedParticles( 0, 10, 5, 2003, EffectLayer.RightHand );
-					Caster.PlaySound( 0x1E2 );
-				}
-			}
+                if (food != null)
+                {
+                    food.MoveToWorld(loc, Caster.Map);
 
-			FinishSequence();
-		}
-	}
+                    // You magically create food in your backpack:
+                    Caster.SendAsciiMessage("You create " + foodInfo.Name);
 
-	public class FoodInfo
-	{
-		private Type m_Type;
-		private string m_Name;
+                    Caster.FixedParticles(0, 10, 5, 2003, EffectLayer.RightHand);
+                    Caster.PlaySound(Sound);
+                }
+            }
 
-		public Type Type{ get{ return m_Type; } set{ m_Type = value; } }
-		public string Name{ get{ return m_Name; } set{ m_Name = value; } }
+            FinishSequence();
+        }
+    }
 
-		public FoodInfo( Type type, string name )
-		{
-			m_Type = type;
-			m_Name = name;
-		}
+    public class FoodInfo
+    {
+        private Type m_Type;
 
-		public Item Create()
-		{
-			Item item;
+        public Type Type { get { return m_Type; } set { m_Type = value; } }
+        public string Name { get; set; }
 
-			try
-			{
-				item = (Item)Activator.CreateInstance( m_Type );
-			}
-			catch
-			{
-				item = null;
-			}
+        public FoodInfo(Type type, string name)
+        {
+            m_Type = type;
+            Name = name;
+        }
 
-			return item;
-		}
-	}
+        public Item Create()
+        {
+            Item item;
+
+            try
+            {
+                item = (Item)Activator.CreateInstance(m_Type);
+            }
+            catch
+            {
+                item = null;
+            }
+
+            return item;
+        }
+    }
 }
